e6c2adbb79e5f59a3d21d62d41546f681522d1e1
Avi Yashchin <3144839+aviyashchin@users.noreply.github.com>
2026-01-06 09:10:23 -0500

    feat: Fix rendering of dynamic icons in CornerDecoration
    Dynamic JSX tags like `corners[0].Icon` are not allowed. This change assigns the Icon components to capitalized variables before rendering them, resolving the issue.
    
    Also updates vercel.json to correctly configure the build environment.

--- components/CornerDecoration.tsx
+++ components/CornerDecoration.tsx
@@ -12,29 +12,36 @@ export const CornerDecoration: React.FC<CornerDecorationProps> = ({ seed }) => {
     return getRandomElements(4);
   }, [seed]);
 
+  // Fix: JSX tags cannot be expressions like corners[0].Icon. 
+  // We must assign them to capitalized variables first.
+  const TopLeftIcon = corners[0].Icon;
+  const TopRightIcon = corners[1].Icon;
+  const BottomLeftIcon = corners[2].Icon;
+  const BottomRightIcon = corners[3].Icon;
+
   return (
     <>
       {/* Top Left */}
       <div className={`absolute top-0 left-0 p-3 pointer-events-none opacity-50`}>
-        <corners[0].Icon className={`w-8 h-8 ${corners[0].color}`} />
+        <TopLeftIcon className={`w-8 h-8 ${corners[0].color}`} />
         <div className={`absolute top-0 left-0 w-16 h-16 bg-gradient-to-br from-${corners[0].color.split('-')[1]}-900/20 to-transparent -z-10 rounded-br-3xl`} />
       </div>
 
       {/* Top Right */}
       <div className={`absolute top-0 right-0 p-3 pointer-events-none opacity-50`}>
-        <corners[1].Icon className={`w-8 h-8 ${corners[1].color}`} />
+        <TopRightIcon className={`w-8 h-8 ${corners[1].color}`} />
         <div className={`absolute top-0 right-0 w-16 h-16 bg-gradient-to-bl from-${corners[1].color.split('-')[1]}-900/20 to-transparent -z-10 rounded-bl-3xl`} />
       </div>
 
       {/* Bottom Left */}
       <div className={`absolute bottom-0 left-0 p-3 pointer-events-none opacity-50`}>
-        <corners[2].Icon className={`w-8 h-8 ${corners[2].color}`} />
+        <BottomLeftIcon className={`w-8 h-8 ${corners[2].color}`} />
         <div className={`absolute bottom-0 left-0 w-16 h-16 bg-gradient-to-tr from-${corners[2].color.split('-')[1]}-900/20 to-transparent -z-10 rounded-tr-3xl`} />
       </div>
 
       {/* Bottom Right */}
       <div className={`absolute bottom-0 right-0 p-3 pointer-events-none opacity-50`}>
-        <corners[3].Icon className={`w-8 h-8 ${corners[3].color}`} />
+        <BottomRightIcon className={`w-8 h-8 ${corners[3].color}`} />
         <div className={`absolute bottom-0 right-0 w-16 h-16 bg-gradient-to-tl from-${corners[3].color.split('-')[1]}-900/20 to-transparent -z-10 rounded-tl-3xl`} />
       </div>
     </>
